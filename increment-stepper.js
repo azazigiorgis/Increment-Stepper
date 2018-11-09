d586af69a4e703c23ba6694ddba890287c096890
azazigiorgis <azazi.giorgis@gmail.com>
2018-11-09 21:26:26 +0100

    Initial commit

---
+++ increment-stepper.js
@@ -0,0 +1,35 @@
+var inc = document.getElementsByClassName("stepper");
+for (i = 0; i < inc.length; i++) {
+  var incI = inc[i].querySelector("input"),
+    id = incI.getAttribute("id"),
+    min = incI.getAttribute("min"),
+    max = incI.getAttribute("max"),
+    step = incI.getAttribute("step");
+  document
+    .getElementById(id)
+    .previousElementSibling.setAttribute(
+      "onclick",
+      "stepperInput('" + id + "', -" + step + ", " + min + ")"
+    ); // Down
+  document
+    .getElementById(id)
+    .nextElementSibling.setAttribute(
+      "onclick",
+      "stepperInput('" + id + "', " + step + ", " + max + ")"
+    ); // Up
+}
+
+// Stepper Increment Function with Min/Max
+function stepperInput(id, s, m) {
+  var el = document.getElementById(id);
+  if (s > 0) {
+    if (parseInt(el.value) < m) {
+      el.value = parseInt(el.value) + s;
+    }
+  } else {
+    if (parseInt(el.value) > m) {
+      el.value = parseInt(el.value) + s;
+    }
+  }
+  // Execute other functions when stepping below
+}
